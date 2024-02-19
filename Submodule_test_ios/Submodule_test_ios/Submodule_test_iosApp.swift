70d93dbd2c4d489ff3a0c44e656c19e159468c57
bh.park <bh.park.dev@gmail.com>
2024-02-19 16:29:34 +0900

    initial commit

---
+++ Submodule_test_ios/Submodule_test_ios/Submodule_test_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Submodule_test_iosApp.swift
+//  Submodule_test_ios
+//
+//  Created by bh.park on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Submodule_test_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
